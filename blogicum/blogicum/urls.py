5e3611957e3c9c0b1d5e8c26623d39094e9efeb4
Ilya Rodman <i.nuyanzin@yandex.ru>
2024-04-08 00:19:36 +0400

    v0.3

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
     path('posts/', include('blog.urls', namespace='post_detail')),
-    #path('category', include('blog.urls', namespace='category_posts')),
+    path('category', include('blog.urls', namespace='category_posts')),
     path('pages/', include('pages.urls', namespace='pages')),
 ]
